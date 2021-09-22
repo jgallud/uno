fbeb316a08867b2b661f9fc5d8ccc39c834fb7f9
José A. Gallud <jgallud@gmail.com>
2021-09-22 17:30:03 +0200

    partidas

--- cliente/modelo.js
+++ cliente/modelo.js
@@ -1,17 +1,28 @@
 
 function Juego(){
     this.jugadores={};
+    this.partidas={};
 
     this.agregarJugador=function(nick){
         if (!this.jugadores[nick]){
-            var jugador= new Jugador(nick);
+            var jugador= new Jugador(nick,this);
             this.jugadores[nick]=jugador;
         }
     }
+
+    this.crearPartida=function(nick,numJug){
+        //crear código único
+        //crear la instancia de Partida
+        //asignarla a la coleccion partidas
+    }
 }
 
-function Jugador(nick){
+function Jugador(nick,juego){
     this.nick=nick;
+    this.juego=juego;
+    this.crearPartida=function(numJug){
+        this.juego.crearPartida(nick,numJug)
+    }
 }
 
 function Partida(nombre){
