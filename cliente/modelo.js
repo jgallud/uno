6cde4759d997aba14065d7fce72fe91df70e5850
José A. Gallud <jgallud@gmail.com>
2021-09-30 19:43:03 +0200

    mano inicial

--- cliente/modelo.js
+++ cliente/modelo.js
@@ -17,9 +17,9 @@ function Juego(){
         var codigo="patata";
         var jugador=this.usuarios[nick];
         codigo=this.obtenerCodigo();
-        // while (this.partidas[codigo]){
-        //     codigo=this.obtenerCodigo();
-        // };
+        while (this.partidas[codigo]){
+            codigo=this.obtenerCodigo();
+        };
         var partida=new Partida(codigo,jugador,numJug);
         this.partidas[codigo]=partida;
 
@@ -45,15 +45,15 @@ function Juego(){
     }
 
     this.obtenerCodigo=function(){
-		// let cadena="ABCDEFGHIJKLMNOPQRSTUVXYZ";
-		// let letras=cadena.split('');
-		// let maxCadena=cadena.length;
-		// let codigo=[];
-		// for(i=0;i<6;i++){
-		// 	codigo.push(letras[randomInt(1,maxCadena)-1]);
-		// }
-		//return codigo.join('');
-        return Date.now().toString();
+		let cadena="ABCDEFGHIJKLMNOPQRSTUVXYZ";
+		let letras=cadena.split('');
+		let maxCadena=cadena.length;
+		let codigo=[];
+		for(i=0;i<6;i++){
+			codigo.push(letras[randomInt(1,maxCadena)-1]);
+		}
+		return codigo.join('');
+        //return Date.now().toString();
 	}
 
     this.numeroPartidas=function(){
@@ -69,17 +69,32 @@ function randomInt(low, high) {
 function Jugador(nick,juego){
     this.nick=nick;
     this.juego=juego;
+    this.mano=[];
+    this.codigoPartida;
     this.crearPartida=function(numJug){
         return this.juego.crearPartida(nick,numJug);
     }
     this.unirAPartida=function(codigo){
         this.juego.unirAPartida(codigo,nick);
     }
+    this.robar=function(num){
+        var partida=this.obtenerPartida(this.codigoPartida);
+        var robadas=partida.dameCartas(num);
+        //var tmp=this.mano;
+        this.mano=this.mano.concat(robadas);
+    }
+    this.manoInicial=function(){
+        var partida=this.obtenerPartida(this.codigoPartida);
+        this.mano=partida.dameCartas(7);
+    }
+    this.obtenerPartida=function(codigo){
+        return this.juego.partidas[codigo];
+    }
 }
 
 function Partida(codigo,jugador,numJug){
     this.codigo=codigo;
-    this.cartas=[];
+    this.mazo=[];
     this.propietario=jugador.nick;
     this.numJug=numJug;
     this.jugadores={};
@@ -90,30 +105,54 @@ function Partida(codigo,jugador,numJug){
     }
     this.puedeUnirAPartida=function(jugador){
         this.jugadores[jugador.nick]=jugador;
+        jugador.codigoPartida=this.codigo;
     }
     this.numeroJugadores=function(){
 		return Object.keys(this.jugadores).length;
 	}
     this.crearMazo=function(){
-        var colores=["azul","amarillo","rojo","verde"];
-
-        for(i=0;i<colores.length;i++){
-            this.cartas.push(new Numero(0,colores[i]));
+        var colores=["azul","amarillo","verde","rojo"];
+        for (i=0;i<colores.length;i++){
+            this.mazo.push(new Numero(0,colores[i]));
         }
-        for(i=0;i<colores.length;i++){
-            for(j=1;j<10;j++){
-                this.cartas.push(new Numero(j,colores[i]));
-                this.cartas.push(new Numero(j,colores[i]));
+        for(j=0;j<colores.length;j++){
+            for (i=1;i<10;i++){
+                this.mazo.push(new Numero(i,colores[j]));
+                this.mazo.push(new Numero(i,colores[j]));
             }
         }
-         for(i=0;i<colores.length;i++){
-            this.cartas.push(new Cambio(20,colores[i]));
+        for(j=0;j<colores.length;j++){
+            this.mazo.push(new Cambio(20,colores[j]));
+            this.mazo.push(new Cambio(20,colores[j]));
         }
+        for(j=0;j<colores.length;j++){
+            this.mazo.push(new Bloqueo(20,colores[j]));
+            this.mazo.push(new Bloqueo(20,colores[j]));
+        }
+        for(j=0;j<colores.length;j++){
+            this.mazo.push(new Mas2(20,colores[j]));
+            this.mazo.push(new Mas2(20,colores[j]));
+        }
+        for (i=1;i<5;i++){
+            this.mazo.push(new Comodin(20));
+            this.mazo.push(new Comodin4(20));
+        }
+    };
 
-        //...
-
-
+    this.asignarUnaCarta=function(){
+        var maxCartas=this.mazo.length;
+        var indice=randomInt(1,maxCartas)-1;
+        var carta=this.mazo.splice(indice,1);
+        return carta[0];
     }
+    this.dameCartas=function(num){
+        var cartas=[];
+        for(i=0;i<num;i++){
+            cartas.push(this.asignarUnaCarta());
+        }
+        return cartas;
+    }
+
     this.crearMazo();
     this.unirAPartida(jugador);
 }
@@ -141,11 +180,35 @@ function Final(){
 }
 
 function Numero(valor,color){
+    this.tipo="numero";
     this.color=color;
     this.valor=valor;
 }
 
 function Cambio(valor,color){
+    this.tipo="cambio";
     this.color=color;
+    this.valor=valor;   
+}
+
+function Bloqueo(valor,color){
+    this.tipo="bloqueo";
+    this.color=color;
+    this.valor=valor;    
+}
+
+function Mas2(valor,color){
+    this.tipo="mas2";
+    this.color=color;
+    this.valor=valor;    
+}
+
+function Comodin(valor){
+    this.tipo="comodin";
+    this.valor=valor;
+}
+
+function Comodin4(valor){
+    this.tipo="comodin4";
     this.valor=valor;
 }
--- cliente/spec/ModeloSpec.js
+++ cliente/spec/ModeloSpec.js
@@ -8,81 +8,95 @@ describe("El juego del UNO...", function() {
     juego.agregarJugador("luis");
   });
 
-  it("Comprobar mazo",function(){
-      var ju1=juego.usuarios["ana"];
+  it("Condiciones iniciales",function(){
       expect(juego.numeroPartidas()).toEqual(0);
       expect(juego.obtenerTodasPartidas().length).toEqual(0);
-      var partida=ju1.crearPartida(2);
-
-      expect(partida.cartas.length).toBe(108);
-      var rojo=partida.cartas.filter(function(each){
-        return each.color=="rojo";
-      });
-      expect(rojo.length).toBe(25);
-      var verde=partida.cartas.filter(function(each){
-        return each.color=="verde";
-      });
-      expect(verde.length).toBe(25);
-      var amarillo=partida.cartas.filter(function(each){
-        return each.color=="amarillo";
-      });
-      expect(amarillo.length).toBe(25);
-      var azul=partida.cartas.filter(function(each){
-        return each.color=="azul";
-      });
-      expect(azul.length).toBe(25);
-      var comodin=partida.cartas.filter(function(each){
-        return each.tipo=="comodin";
-      });
-      expect(comodin.length).toBe(4);
-      var comodin4=partida.cartas.filter(function(each){
-        return each.tipo=="comodin4";
-      });
-      expect(comodin4.length).toBe(4);
   });
 
-  it("Ana crea una partida para 2 jugadores", function() {
-    var ju1=juego.usuarios["ana"];
-    expect(juego.numeroPartidas()).toEqual(0);
-    expect(juego.obtenerTodasPartidas().length).toEqual(0);
-    var partida=ju1.crearPartida(2);
-    expect(juego.numeroPartidas()).toEqual(1);
-    expect(partida.codigo).toBeDefined();
-    expect(partida.numeroJugadores()).toEqual(1);
-    expect(juego.obtenerTodasPartidas().length).toEqual(1);
-    expect(partida.fase.nombre).toBe("inicial");
-  });
+  describe("Ana crea una partida de 2 jugadores...", function() {
+    var ju1;
+    var partida;
 
-  it("Ana crea la partida para 2 jugadores y Pepe se une",function(){
-    var ju1=juego.usuarios["ana"];
-    expect(juego.numeroPartidas()).toEqual(0);
-    var partida=ju1.crearPartida(2);
-    expect(juego.numeroPartidas()).toEqual(1);
-    expect(partida.codigo).toBeDefined();
-    expect(partida.numeroJugadores()).toEqual(1);
-    expect(partida.fase.nombre).toBe("inicial");
-    var ju2=juego.usuarios["pepe"];
-    ju2.unirAPartida(partida.codigo);
-    expect(partida.numeroJugadores()).toEqual(2);
-    expect(partida.fase.nombre).toBe("jugando");
-  });
+    beforeEach(function(){
+      ju1=juego.usuarios["ana"];
+      partida=ju1.crearPartida(2);
+    });
 
-  it("Ana crea la partida para 2 jugadores, Pepe se une, Luis intenta unirse y no puede",function(){
-    var ju1=juego.usuarios["ana"];
-    expect(juego.numeroPartidas()).toEqual(0);
-    var partida=ju1.crearPartida(2);
-    expect(juego.numeroPartidas()).toEqual(1);
-    expect(partida.codigo).toBeDefined();
-    expect(partida.numeroJugadores()).toEqual(1);
-    expect(partida.fase.nombre).toBe("inicial");
-    var ju2=juego.usuarios["pepe"];
-    ju2.unirAPartida(partida.codigo);
-    expect(partida.numeroJugadores()).toEqual(2);
-    expect(partida.fase.nombre).toBe("jugando");
-    var ju3=juego.usuarios["luis"];
-    ju3.unirAPartida(partida.codigo);
-    expect(partida.numeroJugadores()).toEqual(2);
-    expect(partida.fase.nombre).toBe("jugando");
-  });
+    it("Comprobar obtener partida",function(){
+      var codigo=ju1.codigoPartida;
+      expect(ju1.obtenerPartida(codigo)).toBeDefined();
+    });
 
+    it("Comprobar mazo",function(){
+        expect(partida.mazo.length).toBe(108);
+        var rojo=partida.mazo.filter(function(each){
+          return each.color=="rojo";
+        });
+        expect(rojo.length).toBe(25);
+        var verde=partida.mazo.filter(function(each){
+          return each.color=="verde";
+        });
+        expect(verde.length).toBe(25);
+        var amarillo=partida.mazo.filter(function(each){
+          return each.color=="amarillo";
+        });
+        expect(amarillo.length).toBe(25);
+        var azul=partida.mazo.filter(function(each){
+          return each.color=="azul";
+        });
+        expect(azul.length).toBe(25);
+        var comodin=partida.mazo.filter(function(each){
+          return each.tipo=="comodin";
+        });
+        expect(comodin.length).toBe(4);
+        var comodin4=partida.mazo.filter(function(each){
+          return each.tipo=="comodin4";
+        });
+        expect(comodin4.length).toBe(4);
+    });
+
+    it("Comprobamos la partida para 2 jugadores", function() {
+      //var ju1=juego.usuarios["ana"];
+      //expect(juego.numeroPartidas()).toEqual(0);
+      //expect(juego.obtenerTodasPartidas().length).toEqual(0);
+      //var partida=ju1.crearPartida(2);
+      expect(juego.numeroPartidas()).toEqual(1);
+      expect(partida.codigo).toBeDefined();
+      expect(partida.numeroJugadores()).toEqual(1);
+      expect(juego.obtenerTodasPartidas().length).toEqual(1);
+      expect(partida.fase.nombre).toBe("inicial");
+    });
+
+    it("Pepe se une",function(){
+      // var ju1=juego.usuarios["ana"];
+      // expect(juego.numeroPartidas()).toEqual(0);
+      // var partida=ju1.crearPartida(2);
+      // expect(juego.numeroPartidas()).toEqual(1);
+      // expect(partida.codigo).toBeDefined();
+      // expect(partida.numeroJugadores()).toEqual(1);
+      // expect(partida.fase.nombre).toBe("inicial");
+      var ju2=juego.usuarios["pepe"];
+      ju2.unirAPartida(partida.codigo);
+      expect(partida.numeroJugadores()).toEqual(2);
+      expect(partida.fase.nombre).toBe("jugando");
+    });
+
+    it("Pepe se une, Luis intenta unirse y no puede",function(){
+      // var ju1=juego.usuarios["ana"];
+      // expect(juego.numeroPartidas()).toEqual(0);
+      // var partida=ju1.crearPartida(2);
+      // expect(juego.numeroPartidas()).toEqual(1);
+      // expect(partida.codigo).toBeDefined();
+      // expect(partida.numeroJugadores()).toEqual(1);
+      // expect(partida.fase.nombre).toBe("inicial");
+      var ju2=juego.usuarios["pepe"];
+      ju2.unirAPartida(partida.codigo);
+      expect(partida.numeroJugadores()).toEqual(2);
+      expect(partida.fase.nombre).toBe("jugando");
+      var ju3=juego.usuarios["luis"];
+      ju3.unirAPartida(partida.codigo);
+      expect(partida.numeroJugadores()).toEqual(2);
+      expect(partida.fase.nombre).toBe("jugando");
+    });
+  });
 });
